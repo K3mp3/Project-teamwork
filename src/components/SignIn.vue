99d702a634b0af8372b18e72b5857f279c7e9c50
Melvin Kempe <k3mp314@gmail.com>
2023-05-18 14:59:55 +0200

    [Added] the sign in form

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -2,8 +2,24 @@
 
 </script>
 <template>
+    <div class="sign-in-form-container">
+        <form class="sign-in-form">
 
+        </form>
+    </div>
 </template>
-<style>
+<style scoped>
+    .sign-in-form-container {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
 
+    .sign-in-form {
+        width: 350px;
+        height: 450px;
+        background-color: #292424;
+    }
 </style>
