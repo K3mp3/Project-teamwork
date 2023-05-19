b829dd0b22404d605c139f34811170ffe6ff644e
Melvin Kempe <k3mp314@gmail.com>
2023-05-19 12:40:55 +0200

    [Added] input fields to the sign in form

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -2,24 +2,46 @@
 
 </script>
 <template>
-    <div class="sign-in-form-container">
-        <form class="sign-in-form">
+    <form>
+        <label for="uname" class="visually-hidden">Username </label>
+        <input type="text" id="uname" name="uname" placeholder="Username">
 
-        </form>
-    </div>
+        <label for="email" class="visually-hidden">Email </label>
+        <input type="email" id="email" name="email" placeholder="Email">
+
+        <label for="pword" class="visually-hidden">Password </label>
+        <input type="password" id="pword" name="pword" placeholder="password">
+    </form>
 </template>
 <style scoped>
-    .sign-in-form-container {
-        width: 100vw;
-        height: 100vh;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-    }
+form {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    gap: 40px;
+    align-items: center;
+}
+.visually-hidden {
+    position: absolute;
+    position: absolute !important;
+    width: 1px !important;
+    height: 1px !important;
+    padding: 0 !important;
+    margin: -1px !important;
+    overflow: hidden !important;
+    clip: rect(0, 0, 0, 0) !important;
+    white-space: nowrap !important;
+    border: 0 !important;
+}
 
-    .sign-in-form {
-        width: 350px;
-        height: 450px;
-        background-color: #292424;
-    }
+input {
+    width: 70%;
+    height: 40px;
+    margin: auto;
+    border: none;
+    border-radius: 10px;
+    box-sizing: border-box;
+    padding-left: 10px;
+    background-color: #ebebeb;
+}
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,34 @@
 <script setup lang="ts">
-import SignIn from '@/components/SignIn.vue';
+  import SignIn from '@/components/SignIn.vue';
 </script>
 
 <template>
   <main>
-    <SignIn>
+      <div class="sign-in-form-container">
+        <form class="sign-in-form"> 
+          <SignIn>
 
-    </SignIn>
+          </SignIn>
+        </form>
+      </div>
   </main>
 </template>
+
+<style scoped>
+  .sign-in-form-container {
+        width: 100vw;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+    .sign-in-form {
+        width: 350px;
+        height: 450px;
+        background-color: #292424;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+</style>
