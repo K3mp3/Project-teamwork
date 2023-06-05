077c76b7d1c31fbb598249988cc5e413098a0e08
Melvin Kempe <k3mp314@gmail.com>
2023-06-05 16:29:24 +0200

    [Changed] the li-tags so that it also contains an a-tag to create better accessibility. [Added] an @click function to the a-tag and the link parameter to be able to see which a-tag that the user clicked on

--- src/components/startpage/StartPageAsideLinks.vue
+++ src/components/startpage/StartPageAsideLinks.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
     const links: string[] = ["Create room", "About", "Contact", "FAQ"];
+
+    function handleClick(link: string) {
+        const trimmedLink = link.trim().replace(/\s/g, '');
+        console.log("link", trimmedLink.toLowerCase());
+    }
 </script>
 <template>
     <aside>
         <ul>
-            <li v-for="link in links">{{ link }}</li>
+            <li v-for="link in links"><a @click="handleClick(link)">{{ link }}</a></li>
         </ul>
     </aside>
 </template>
